5a85f802a5aa45f5693423d27d5a659490650390
Axel-LiraR <99221025+Axel-LiraR@users.noreply.github.com>
2022-03-02 21:56:46 -0800

    Create Practice.js

---
+++ Practice.js
@@ -0,0 +1,8 @@
+console.log('This is practice code to practice commits')
+
+function basicSquareCalc(numToBeSquared){
+    numSquared = numToBeSquared * numToBeSquared;
+    console.log(numToBeSquared + ' squared is ' + numSquared);
+}
+
+basicSquareCalc(5);
