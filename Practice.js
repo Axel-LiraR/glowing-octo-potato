a83aea8e2664c1eb4148f733e97532d6a322ede0
Axel-LiraR <99221025+Axel-LiraR@users.noreply.github.com>
2022-03-02 22:11:26 -0800

    Deleted to restart

--- Practice.js
+++
@@ -1,8 +0,0 @@
-console.log('This is practice code to practice commits')
-
-function basicSquareCalc(numToBeSquared){
-    numSquared = numToBeSquared * numToBeSquared;
-    console.log(numToBeSquared + ' squared is ' + numSquared);
-}
-
-basicSquareCalc(5);
