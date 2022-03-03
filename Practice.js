d3aea10373263736f71e41a197f6dc1a5382371f
Axel-LiraR <99221025+Axel-LiraR@users.noreply.github.com>
2022-03-02 22:22:15 -0800

    Upload Practice.js file
    uploaded first file

--- Practice.js
+++
@@ -1,6 +0,0 @@
-//This is a basic Square Calculator to practice pushing commits
-function numToSquare(number){
-    Squarednumber = number * number;
-    console.log(number + ' squared is ' + Squarednumber);
-}
-numToSquare(5);
---
+++ Practice/Practice.js
@@ -0,0 +1,6 @@
+//Squaring Calcultor
+function Square(number){
+    squaredNum = number * number;
+    console.log(number + ' squared is ' + squaredNum);
+}
+Square(5);
